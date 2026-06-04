620158594cea03b456e0153e785104f8dcaf8651
saguilar4891 <amsebastian12@gmail.com>
2026-06-04 12:42:55 -0400

    Forgot to save customerSearchPage.jsx

--- src/pages/customerSearchPage.jsx
+++ src/pages/customerSearchPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navBar";
 import { getCustomerById } from "../services/bankApi";
 
 function CustomerSearchPage() {
