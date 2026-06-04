61f8a4d7386dc165988b4244958ad87f1acfadba
saguilar4891 <amsebastian12@gmail.com>
2026-06-04 14:41:44 -0400

    Changed API_URL to reflect deployed backend address

--- src/services/bankApi.js
+++ src/services/bankApi.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5203/api";
+const API_URL = "https://citibank-projects.onrender.com/api";
 
 export async function getCustomers() {
     const response = await fetch(`${API_URL}/customers`);
