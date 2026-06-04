66516d80cb610e884b3cd8ea1386f029268796d2
saguilar4891 <amsebastian12@gmail.com>
2026-06-04 12:40:50 -0400

    Fixed NavBar to navBar issue

--- src/pages/customersPage.jsx
+++ src/pages/customersPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Navbar from "../components/Navbar";
+import Navbar from "../components/navBar";
 import { getCustomers } from "../services/bankApi";
 
 function CustomersPage() {
